2b02651f6036a22befe6d6b9b43492baf198ba2f
Moss <ramivalmont@gmail.com>
2021-06-03 17:31:40 +1200

    Add basic authentication to add articles and delete articles and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
@@ -9,7 +12,7 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
-    
+
     redirect_to article_path(@article)
   end
 
